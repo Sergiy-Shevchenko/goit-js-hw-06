19837e25d37f97bc3b33633ba5a5d5c2d2e7e122
Sergiy-Shevchenko <116056626+Sergiy-Shevchenko@users.noreply.github.com>
2023-02-15 23:46:16 +0200

    1-2DZ

---
+++ src/js/task-02.js
@@ -0,0 +1,26 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ingredientsEl = document.querySelector('#ingredients');
+//console.log(ingredientsEl);
+
+
+
+const elements = ingredients.map(option => {
+const liItemEl = document.createElement('li');
+liItemEl.classList.add('item');
+liItemEl.textContent = option;
+
+return liItemEl;
+})
+
+//console.log(elements);
+
+ingredientsEl.append(...elements);
+
